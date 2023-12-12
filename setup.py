15a4a608760b9cfc6f64b8c1510b031c92f906c2
gtluubruker <138827447+gtluubruker@users.noreply.github.com>
2023-12-12 13:36:16 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='scils_segment_class',
       author_email='Gordon.Luu@Bruker.com',
       license='MIT License',
       packages=['bin'],
-      entry_points={'console_scripts': ['segment_class=bin.scils_segment_class.py:main']},
+      entry_points={'console_scripts': ['segment_class=bin.scils_segment_class:main']},
       install_requires=['numpy', 'pandas', 'scikit-image'])
